7014906e1b79c4c787cc66d5d7c371b81e5bf4aa
haider-zada <haiderzada96@hotmail.com>
2020-05-14 12:42:19 +0500

    py test added

---
+++ books/unit_tests/books_pytest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestNation:
+
+    def test_one(self):
+        assert 400 == 400
+
+    def test_two(self):
+        assert 400 == 400
+
+    def test_three(self):
+        assert 400 == 400
